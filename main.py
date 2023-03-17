b9c7bca1a440f2cafbb04376d7910c0b0537eb4d
Wayn <84714801+uaerious@users.noreply.github.com>
2023-03-17 12:49:54 +0000

    SUPER IMPORTANT PRIVACY CHANGES

--- main.py
+++ main.py
@@ -1,36 +1,51 @@
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request, redirect, session
 
 app = Flask(__name__)
+app.secret_key = 'your_secret_key'
 
-SEATS = ['Seat 1', 'Seat 2', 'Seat 3', 'Seat 4', 'Seat 5']
+seats = ['A1', 'A2', 'A3', 'B1', 'B2', 'B3']
+timeslots = ['10:00', '12:00', '14:00', '16:00']
 
-# Initialize a dictionary to store seat bookings
+# Dictionary to store bookings
 bookings = {}
 
 @app.route('/')
 def index():
-    return render_template('index.html', seats=SEATS)
+	return render_template('index.html', seats=seats, timeslots=timeslots)
 
 @app.route('/book', methods=['POST'])
 def book_seat():
-    if request.method == 'POST':
-        # Get form data from request
-        name = request.form['name']
-        email = request.form['email']
-        seat = request.form['seat']
-        date = request.form['date']
-        time = request.form['time']
+	name = request.form['name']
+	email = request.form['email']
+	seat = request.form['seat']
+	date = request.form['date']
+	timeslot = request.form['timeslot']
 
-    if seat in bookings:
-        message = f'Seat {seat} is already booked. Please choose another seat.'
-        return render_template('error.html', message=message)
-    else:
-        # Add booking to dictionary
-        bookings[seat] = {'name': name, 'email': email, 'date': date, 'time': time}
-        return redirect('/confirm')
+	# Check if seat is available for the chosen date and timeslot
+	if bookings.get(date) and bookings[date].get(timeslot) and seat in bookings[date][timeslot]:
+		message = f'Seat {seat} is already booked for the selected date and timeslot. Please choose another seat or select a different date/timeslot.'
+		return render_template('error.html', message=message)
 
-@app.route('/confirm')
-def confirm():
-    return render_template('confirm.html', bookings=bookings)
+	# Add booking to dictionary
+	if date not in bookings:
+		bookings[date] = {}
+	if timeslot not in bookings[date]:
+		bookings[date][timeslot] = []
+	bookings[date][timeslot].append(seat)
+
+	# Store the booking information in a session variable
+	session['booking'] = {'name': name, 'email': email, 'seat': seat, 'date': date, 'timeslot': timeslot}
+
+	return redirect('/confirmation')
+
+@app.route('/confirmation')
+def confirmation():
+	# Retrieve the booking information from the session variable
+	booking = session.get('booking')
+
+	if booking:
+		return render_template('confirmation.html', booking=booking)
+	else:
+		return render_template('error.html')
 
 app.run(host='0.0.0.0', port=81)
