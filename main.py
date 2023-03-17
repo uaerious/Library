b69aa5b7f9f63efcc2be7fd8ee46112241d7082f
Wayn <84714801+uaerious@users.noreply.github.com>
2023-03-17 08:33:05 +0000

    date

--- main.py
+++ main.py
@@ -2,10 +2,9 @@ from flask import Flask, render_template, request, redirect
 
 app = Flask(__name__)
 
-# Available seats
 SEATS = ['Seat 1', 'Seat 2', 'Seat 3', 'Seat 4', 'Seat 5']
 
-# Bookings dictionary
+# Initialize a dictionary to store seat bookings
 bookings = {}
 
 @app.route('/')
@@ -13,19 +12,20 @@ def index():
     return render_template('index.html', seats=SEATS)
 
 @app.route('/book', methods=['POST'])
-def book():
-    # Get form data
-    name = request.form['name']
-    email = request.form['email']
-    seat = request.form['seat']
+def book_seat():
+    if request.method == 'POST':
+        # Get form data from request
+        name = request.form['name']
+        email = request.form['email']
+        seat = request.form['seat']
+        time = request.form['time']
 
-    # Check if seat is available
     if seat in bookings:
         message = f'Seat {seat} is already booked. Please choose another seat.'
         return render_template('error.html', message=message)
     else:
         # Add booking to dictionary
-        bookings[seat] = {'name': name, 'email': email}
+        bookings[seat] = {'name': name, 'email': email, 'time': time}
         return redirect('/confirm')
 
 @app.route('/confirm')
